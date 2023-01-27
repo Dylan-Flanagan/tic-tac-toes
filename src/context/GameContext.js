1244e11ee689c29629cdb6b7309562dc50e3dd87
Dylan <dylflanagan@gmail.com>
2023-01-26 16:43:22 -0800

    State variables built out in GameContext.js

---
+++ src/context/GameContext.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const { useContext } = require('react');
+const { createContext } = require('react');
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [currentPlayer, setCurrentPlayer] = useState('x'); //'x' or 'o'
+  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']); // showcasing taken and open boxes
+  const [active, setActive] = useState('true'); // boolean
+  const [gameMessage, setGameMessage] = useState('Your turn: X'); // 'Your turn __' or 'You Win __'
+
+  return (
+    <GameContext.Provider
+      value={{
+        currentPlayer,
+        setCurrentPlayer,
+        board,
+        setBoard,
+        active,
+        setActive,
+        gameMessage,
+        setGameMessage,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  if (context === undefined) {
+    throw new Error('useGameContext must be used within a GameProvider');
+  }
+  return context;
+};
+
+export { GameProvider, useGameContext };
